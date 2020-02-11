e6bfb987e24e3deaa598d39b1f76fd5cb0d95145
Fabricio <fabricio.a.s@hotmail.com>
2020-02-11 20:03:38 -0300

    mudança em provider

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        JasperPHP\JasperPHPServiceProvider::class,
 
         /*
          * Package Service Providers...
